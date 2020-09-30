f20c5be1f6e440ad229d5d74ff779133796c1d52
Pawel Bozejewski <bozejewski@gmail.com>
2020-09-30 10:01:05 +0200

    test3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <HashRouter basename="/">
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <div className="homeContainer" id="main">
+      test2
+    </div>
+  );
+};
+
+export default Home;
