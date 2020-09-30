d998a82941472f5d26c24f339d92b69218fc6d3d
Pawel Bozejewski <bozejewski@gmail.com>
2020-09-30 15:39:41 +0200

    SUCCESS

--- src/Components/Home/OurMission/ourMission.js
+++ src/Components/Home/OurMission/ourMission.js
@@ -10,8 +10,18 @@ import img7 from '../../../assets/bg7.jpg';
 import img8 from '../../../assets/bg8.jpg';
 
 const listItems = [
-  { index: 1, text: 'Kontakt telefoniczny', src: img1, cName: 'image1' },
-  { index: 2, text: 'Umówienie spotkania', src: img2, cName: 'image2' },
+  {
+    index: 1,
+    text: 'Kontakt telefoniczny',
+    src: img1,
+    cName: 'image1',
+  },
+  {
+    index: 2,
+    text: 'Umówienie spotkania',
+    src: img2,
+    cName: 'image2',
+  },
   {
     index: 3,
     text: 'Wykonanie audytu energetycznego',
@@ -36,8 +46,18 @@ const listItems = [
     src: img6,
     cName: 'image6',
   },
-  { index: 7, text: 'Podpisanie umowy ', src: img7, cName: 'image7' },
-  { index: 8, text: 'Oszczędność energetyczna ', src: img8, cName: 'image8' },
+  {
+    index: 7,
+    text: 'Podpisanie umowy ',
+    src: img7,
+    cName: 'image7',
+  },
+  {
+    index: 8,
+    text: 'Oszczędność energetyczna ',
+    src: img8,
+    cName: 'image8',
+  },
 ];
 
 const OurMission = () => {
@@ -48,42 +68,8 @@ const OurMission = () => {
     setIsHovering(false);
   };
 
-  const handleSetImage = () => {
-    setImageSrc(listItems[0].src);
-    setIsHovering(true);
-  };
-
-  const handleSetImage1 = () => {
-    setImageSrc(listItems[1].src);
-    setIsHovering(true);
-  };
-
-  const handleSetImage2 = () => {
-    setImageSrc(listItems[2].src);
-    setIsHovering(true);
-  };
-
-  const handleSetImage3 = () => {
-    setImageSrc(listItems[3].src);
-    setIsHovering(true);
-  };
-
-  const handleSetImage4 = () => {
-    setImageSrc(listItems[4].src);
-    setIsHovering(true);
-  };
-
-  const handleSetImage5 = () => {
-    setImageSrc(listItems[5].src);
-    setIsHovering(true);
-  };
-
-  const handleSetImage6 = () => {
-    setImageSrc(listItems[6].src);
-    setIsHovering(true);
-  };
-  const handleSetImage7 = () => {
-    setImageSrc(listItems[7].src);
+  const handleSetImage = (sorce) => {
+    setImageSrc(sorce);
     setIsHovering(true);
   };
 
@@ -93,70 +79,16 @@ const OurMission = () => {
         <div className="listHolder">
           <div className="titleHolder">Jak wygląda współpraca z nami</div>
           <ul>
-            <li>
-              <p
-                onMouseLeave={handleOnMouseLeave}
-                onMouseEnter={handleSetImage}
-              >
-                {listItems[0].text}
-              </p>
-            </li>
-            <li>
-              <p
-                onMouseLeave={handleOnMouseLeave}
-                onMouseEnter={handleSetImage1}
-              >
-                {listItems[1].text}
-              </p>
-            </li>
-            <li>
-              <p
-                onMouseLeave={handleOnMouseLeave}
-                onMouseEnter={handleSetImage2}
-              >
-                {listItems[2].text}
-              </p>
-            </li>
-            <li>
-              <p
-                onMouseLeave={handleOnMouseLeave}
-                onMouseEnter={handleSetImage3}
-              >
-                {listItems[3].text}
-              </p>
-            </li>
-            <li>
-              <p
-                onMouseLeave={handleOnMouseLeave}
-                onMouseEnter={handleSetImage4}
-              >
-                {listItems[4].text}
-              </p>
-            </li>
-            <li>
-              <p
-                onMouseLeave={handleOnMouseLeave}
-                onMouseEnter={handleSetImage5}
-              >
-                {listItems[5].text}
-              </p>
-            </li>
-            <li>
-              <p
-                onMouseLeave={handleOnMouseLeave}
-                onMouseEnter={handleSetImage6}
-              >
-                {listItems[6].text}
-              </p>
-            </li>
-            <li>
-              <p
-                onMouseLeave={handleOnMouseLeave}
-                onMouseEnter={handleSetImage7}
-              >
-                {listItems[7].text}
-              </p>
-            </li>
+            {listItems.map((obj) => (
+              <li key={obj.index}>
+                <p
+                  onMouseLeave={handleOnMouseLeave}
+                  onMouseEnter={() => handleSetImage(obj.src)}
+                >
+                  {obj.text}
+                </p>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
@@ -169,15 +101,3 @@ const OurMission = () => {
 };
 
 export default OurMission;
-
-/* <ul>
-            {listItems.map((obj, index) => (
-              <li
-                key={index}
-                onMouseEnter={(handleOnMouseEnter, handleTest(obj.text))}
-                onMouseLeave={handleOnMouseLeave}
-              >
-                <p>{obj.text}</p>
-              </li>
-            ))}
-          </ul> */
