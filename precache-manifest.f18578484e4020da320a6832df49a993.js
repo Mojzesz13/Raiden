256a2e3d13405240f231897893d0a73b6cf90914
Pawel Bozejewski <bozejewski@gmail.com>
2020-09-30 10:04:18 +0200

    Updates

---
+++ precache-manifest.f18578484e4020da320a6832df49a993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d9650e047f75dfe3d325aeb59e5831",
+    "url": "/raiden2/index.html"
+  },
+  {
+    "revision": "e835060087b65d1fc468",
+    "url": "/raiden2/static/css/main.9270e184.chunk.css"
+  },
+  {
+    "revision": "2eb192c2025a7a3d73f8",
+    "url": "/raiden2/static/js/2.a64f5c01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/raiden2/static/js/2.a64f5c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e835060087b65d1fc468",
+    "url": "/raiden2/static/js/main.29cfeeab.chunk.js"
+  },
+  {
+    "revision": "2dc90ec6f0a42d3c2b92",
+    "url": "/raiden2/static/js/runtime-main.564aee2d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.7029734c32e3f5538f4dbf569702c7a9.js"
+  "/raiden2/precache-manifest.f18578484e4020da320a6832df49a993.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/raiden2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
